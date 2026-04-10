3a3eba3b4a5888cc4ff3d0237f7007d6c44de281
bapotuse <beauxbaptiste@gmail.com>
2026-04-10 19:59:25 +0200

    Debut de page d'accueil

---
+++ frontend/src/Home.vue
@@ -0,0 +1,26 @@
+<template>
+    <nav>
+        <router-link to="/">Didier & Brigitte</router-link>
+        <div class="liens">
+            <router-link to="/click-and-collect">Click and Collect</router-link>
+            <router-link to="/contact">Contact</router-link>
+        </div>
+    </nav>
+</template>
+
+<style scoped>
+nav {
+    display: flex;
+    font-size: 30px;
+}
+
+.liens a{
+    margin-right:50px;
+}
+
+nav a {
+    text-decoration: none;
+    color: black;
+    margin-right:50px;
+}
+</style>
